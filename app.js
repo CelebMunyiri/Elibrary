b2a5d692d889f7d99f873eabe10be2ab7fad536a
David Munyiri <davidmumyiri2019@gmail.com>
2023-01-30 06:08:48 -0800

    first commit in the project

---
+++ app.js
@@ -0,0 +1,5 @@
+const buttonBackgroundColor = document.querySelector(".color");
+const body = document.querySelector(".body");
+buttonBackgroundColor.addEventListener("click", function () {
+  nav.style.BackgroundColor = "blue";
+});
