5c03f2bebe231a44ad5252bbf6e33322dfbe6362
David Munyiri <davidmumyiri2019@gmail.com>
2023-02-06 03:17:33 -0800

    commenced on js implemetation

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+"use strict";
+
 const buttonBackgroundColor = document.querySelector(".color");
 const body = document.querySelector(".body");
 buttonBackgroundColor.addEventListener("click", function () {
-  nav.style.BackgroundColor = "blue";
+  body.style.backGroundColor = "blue";
 });
