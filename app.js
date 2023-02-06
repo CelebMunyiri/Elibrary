60a34a1e7188ef566cde4b1ac8e105523a47ddf9
David Munyiri <davidmumyiri2019@gmail.com>
2023-02-06 04:38:52 -0800

    completed on implemernting dark mode

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 "use strict";
+const body = document.querySelector("#body"); //get the body element
+const buttonColor = document.querySelector(".color"); //get the button for switching background color element
+//set initial theme according to local storage or default
+let currentTheme = localStorage.getItem("theme") || "light";
 
-const buttonBackgroundColor = document.querySelector(".color");
-const body = document.querySelector(".body");
-buttonBackgroundColor.addEventListener("click", function () {
-  body.style.backGroundColor = "blue";
+//set body class to current theme
+body.style.backgroundColor = currentTheme === "light" ? "white" : "black";
+
+//update button text based on the current theme
+buttonColor.innerHTML = currentTheme === "light" ? "Dark Mode" : "Light Mode";
+
+//add an event listener to the button to toggle the color theme
+buttonColor.addEventListener("click", () => {
+  //update body background color to oppositr theme
+  body.style.backgroundColor = currentTheme === "light" ? "black" : "white";
+
+  //update current theme to opposite theme
+  currentTheme = currentTheme === "light" ? "dark" : "light";
+
+  //Update the button text
+  buttonColor.innerHTML = currentTheme === "light" ? "Dark Mode" : "Light Mode";
+
+  localStorage.setItem("theme", currentTheme);
 });
