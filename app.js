767b65bc2b037e90c6cc56d3253e444979f5fb9f
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-11 11:57:43 +0300

    consumed the google books API to get more books

--- app.js
+++ app.js
@@ -1,53 +1,159 @@
-"use strict";
-const body = document.querySelector("#body"); //get the body element
-const buttonColor = document.querySelector(".color"); //get the button for switching background color element
-//set initial theme according to local storage or default
-let currentTheme = localStorage.getItem("theme") || "light";
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const mysql = require("mysql");
 
-//set body class to current theme
-body.style.backgroundColor = currentTheme === "light" ? "white" : "black";
+const app = express();
+app.use(express.static(__dirname + "/index.html"));
 
-//update button text based on the current theme
-buttonColor.innerHTML = currentTheme === "light" ? "Dark Mode" : "Light Mode";
-
-//add an event listener to the button to toggle the color theme
-buttonColor.addEventListener("click", () => {
-  //update body background color to oppositr theme
-  body.style.backgroundColor = currentTheme === "light" ? "black" : "white";
-
-  //update current theme to opposite theme
-  currentTheme = currentTheme === "light" ? "dark" : "light";
-
-  //Update the button text
-  buttonColor.innerHTML = currentTheme === "light" ? "Dark Mode" : "Light Mode";
-
-  localStorage.setItem("theme", currentTheme);
+// Create MySQL Connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "userlogin",
 });
-//const downloads = document.querySelector(".downloads");
-//const read = document.querySelector(".read");
-//const download = document.querySelector(".download");
-var downloadButtons = document.querySelectorAll(".download");
-var readButtons = document.querySelectorAll(".read");
-var downloadsList = document.querySelector(".downloads");
 
-downloadButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    var bookTitle = this.parentNode.parentNode.querySelector("h2").textContent;
-    var downloadLink = document.createElement("a");
-    downloadLink.textContent = bookTitle;
-    downloadLink.href = "#";
-    downloadLink.addEventListener("click", function () {
-      alert("Book downloaded");
-    });
-    var listItem = document.createElement("li");
-    listItem.appendChild(downloadLink);
-    downloadsList.appendChild(listItem);
+// Connect to MySQL
+connection.connect((error) => {
+  if (error) {
+    console.log("Error connecting to MySQL database:", error);
+  } else {
+    console.log("Connected to MySQL database.");
+  }
+});
+
+// Set up the middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "Munyiri@1",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Define routes
+app.get("/", (req, res) => {
+  res.sendFile(_dirname + "/index.html");
+});
+
+// Register User
+app.post("/", (req, res) => {
+  const { name, email, password } = req.body;
+
+  connection.query(
+    "INSERT INTO userlog (name, email, password) VALUES (?, ?, ?)",
+    [name, email, password],
+    (error, results) => {
+      if (error) {
+        console.log("Error registering user:", error);
+        res.sendStatus(500);
+      } else {
+        console.log("User registered successfully:", results);
+        res.sendStatus(200);
+      }
+    }
+  );
+});
+
+// Login User
+app.post("/", (req, res) => {
+  const { email, password } = req.body;
+
+  connection.query(
+    "SELECT * FROM userlog WHERE email = ? AND password = ?",
+    [email, password],
+    (error, results) => {
+      if (error) {
+        console.log("Error logging in:", error);
+        res.sendStatus(500);
+      } else if (results.length === 0) {
+        console.log("Incorrect email or password.");
+        res.sendStatus(401);
+      } else {
+        console.log("User logged in successfully:", results);
+        req.session.user = results[0];
+        res.sendStatus(200);
+      }
+    }
+  );
+});
+
+// Logout User
+app.post("/index", (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.log("Error logging out:", error);
+      res.sendStatus(500);
+    } else {
+      console.log("User logged out successfully.");
+      res.sendStatus(200);
+    }
   });
 });
 
-readButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    var bookTitle = this.parentNode.parentNode.querySelector("h2").textContent;
-    alert("Reading " + bookTitle);
-  });
+// Start the server
+app.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
 });
+
+//CREATE TABLE books (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  description TEXT,
+  publication_date DATE,
+  cover_image VARCHAR(255)
+);
+
+//<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Library Website</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1>Library Website</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Description</th>
+          <th>Publication Date</th>
+          <th>Cover Image</th>
+        </tr>
+      </thead>
+      <tbody id="booksTableBody"></tbody>
+    </table>
+    <script src="script.js"></script>
+  </body>
+</html>
+
+///</head><!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Library Website</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1>Library Website</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Description</th>
+          <th>Publication Date</th>
+          <th>Cover Image</th>
+        </tr>
+      </thead>
+      <tbody id="booksTableBody"></tbody>
+    </table>
+    <script src="script.js"></script>
+  </body>
+</html>
